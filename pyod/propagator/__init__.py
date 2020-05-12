01a5dea7fad4ad94f6842e302795ec74301397de
danielk <daniel.kastinen@irf.se>
2020-05-12 16:40:28 +0200

    adding propagator subpackage

---
+++ pyod/propagator/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''
+
+'''
+
+from .base import Propagator
+
+__all__ = [
+    'Propagator',
+]
+
+try:
+    from .orekit import PropagatorOrekit
+    __all__.append('PropagatorOrekit')
+except ImportError as e:
+    PropagatorOrekit = None
