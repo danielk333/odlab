3309e26075cb4355c562509056df48e3c5866499
danielk <daniel.kastinen@irf.se>
2020-05-15 16:15:14 +0200

    starting to add unit tests

--- tests/test_ccsds.py
+++ tests/test_ccsds.py
@@ -1,4 +1,20 @@
-if __name__=='__main__':
-    ccsds_file = './data/uhf_test_data/events/2002-009A-1473150428.tdm'
-    data = read_ccsds(ccsds_file)
-    print(data)
+#!/usr/bin/env python
+
+'''Test CCSDS data format load functions
+
+'''
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+
+import unittest
+import numpy as np
+import numpy.testing as nt
+
+
+class TestReadCCSDS(unittest.TestCase):
+    # ccsds_file = './data/uhf_test_data/events/2002-009A-1473150428.tdm'
+    # data = read_ccsds(ccsds_file)
+    # print(data)
+    pass
