8a0e167545b3f19bac34cc8445a0e71edb2bd63a
danielk <daniel.kastinen@irf.se>
2022-03-11 10:15:25 +0100

    update example

--- examples/least_sq_posterior.py
+++ examples/least_sq_posterior.py
@@ -4,37 +4,21 @@
 
 '''
 
-import pyod
-from pyod import RadarPair
-from pyod import OptimizeLeastSquares
-from pyod import SourceCollection
-from pyod import SourcePath
-from pyod.plot import orbits, residuals
-from pyod.datetime import mjd2npdt
-from pyod.coordinates import geodetic2ecef
-from pyod.sources import RadarTracklet
+import odlab
+from sorts.frames import geodetic_to_ITRS
 
 import pyorb
 
-import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
 
-# orekit_data = '/home/danielk/IRF/IRF_GITLAB/orekit_build/orekit-data-master.zip'
-# prop = pyod.propagator.Orekit(
-#     orekit_data = orekit_data, 
-#     settings=dict(
-#         in_frame='ITRS',
-#         out_frame='ITRS',
-#         drag_force=True,
-#         radiation_pressure=False,
-#     )
-# )
 
 np.random.seed(89782364)
 
-#how to build a area-to-mass-ratio log10 based propagator
-class MySGP4(pyod.propagator.SGP4):
+# How to build a area-to-mass-ratio log10 based propagator
+
+
+class MySGP4(odlab.propagator.SGP4):
 
     def propagate(self, t, state0, epoch, **kwargs):
         A_div_m = 10.0**kwargs['log10A_div_m']
@@ -54,18 +38,19 @@ prop = MySGP4(
 A = 2.0
 m = 1.0
 
-state0 = np.array([-7100297.113,-3897715.442,18568433.707,86.771,-3407.231,2961.571, np.log10(A/m)])
-t = np.linspace(0,1800/(3600*24),num=10)
+state0 = np.array([-7100297.113, -3897715.442, 18568433.707,
+                   86.771, -3407.231, 2961.571, np.log10(A/m)])
+t = np.linspace(0, 1800/(3600*24), num=10)
 mjd0 = 54952.08
-dates = mjd2npdt(mjd0 + t)
+dates = odlab.datetime.mjd2npdt(mjd0 + t)
 params = dict(C_D=2.3, M0=pyorb.M_earth)
 
 r_err = 1e3
 v_err = 1e2
 
-ski_ecef = geodetic2ecef(69.34023844, 20.313166, 0.0)
-kar_ecef = geodetic2ecef(68.463862, 22.458859, 0.0)
-kai_ecef = geodetic2ecef(68.148205, 19.769894, 0.0)
+ski_ecef = geodetic_to_ITRS(69.34023844, 20.313166, 0.0)
+kar_ecef = geodetic_to_ITRS(68.463862, 22.458859, 0.0)
+kai_ecef = geodetic_to_ITRS(68.148205, 19.769894, 0.0)
 
 rx_list = [ski_ecef, kar_ecef, kai_ecef]
 
@@ -74,14 +59,14 @@ source_data = []
 for rx in rx_list:
     data = dict(
         date = dates,
-        date0 = mjd2npdt(mjd0),
+        date0 = odlab.datetime.mjd2npdt(mjd0),
         tx_ecef = ski_ecef,
         rx_ecef = rx,
     )
-    radar = pyod.RadarPair(data, prop)
+    radar = odlab.RadarPair(data, prop)
     sim_data = radar.evaluate(state0[:6], log10A_div_m=state0[6], **params)
 
-    radar_data = np.empty((len(t),), dtype=RadarTracklet.dtype)
+    radar_data = np.empty((len(t),), dtype=odlab.RadarTracklet.dtype)
     radar_data['date'] = dates
     radar_data['r'] = sim_data['r'] + np.random.randn(len(t))*r_err
     radar_data['v'] = sim_data['v'] + np.random.randn(len(t))*v_err
@@ -95,12 +80,12 @@ for rx in rx_list:
                 rx_ecef = rx,
             ),
             'index': 1,
-        }
+    }
     )
 
-paths = SourcePath.from_list(source_data, 'ram')
+paths = odlab.SourcePath.from_list(source_data, 'ram')
 
-sources = SourceCollection(paths = paths)
+sources = odlab.SourceCollection(paths = paths)
 sources.details()
 
 
@@ -117,12 +102,12 @@ for ind, name in enumerate(variables):
 
 input_data_state = {
     'sources': sources,
-    'Models': [RadarPair]*len(sources),
-    'date0': mjd2npdt(mjd0),
+    'Models': [odlab.RadarPair]*len(sources),
+    'date0': odlab.datetime.mjd2npdt(mjd0),
     'params': params,
 }
 
-post = OptimizeLeastSquares(
+post = odlab.OptimizeLeastSquares(
     data = input_data_state,
     variables = variables,
     state_variables = state_variables,
@@ -151,21 +136,26 @@ print(post.results)
 print('Start error:')
 for var in variables:
     if var in state_variables:
-        print('{:<3}: {:.3f}'.format(var, (state0_named[var][0] - true_state[var][0])*1e-3))
+        print('{:<3}: {:.3f}'.format(
+            var, (state0_named[var][0] - true_state[var][0])*1e-3))
     else:
-        print('{:<3}: {:.3f}'.format(var, state0_named[var][0] - true_state[var][0]))
+        print('{:<3}: {:.3f}'.format(
+            var, state0_named[var][0] - true_state[var][0]))
 
 
 print('\n\nTrue error:')
 for var in variables:
     if var in state_variables:
-        print('{:<3}: {:.3f}'.format(var, (post.results.MAP[var][0] - true_state[var][0])*1e-3))
+        print('{:<3}: {:.3f}'.format(
+            var, (post.results.MAP[var][0] - true_state[var][0])*1e-3))
     else:
-        print('{:<3}: {:.3f}'.format(var, post.results.MAP[var][0] - true_state[var][0]))
+        print('{:<3}: {:.3f}'.format(
+            var, post.results.MAP[var][0] - true_state[var][0]))
 
-# orbits(post, true=true_state)
-# residuals(post, [state0_named, true_state,post.results.MAP], ['Start', 'True', 'MAP'], ['-b', '-r', '-g'], absolute=False)
-residuals(post, [state0_named, true_state,post.results.MAP], ['Start', 'True', 'MAP'], ['-b', '-r', '-g'], absolute=True)
+# odlab.plot.orbits(post, true=true_state)
+# odlab.plot.residuals(post, [state0_named, true_state, post.results.MAP], [
+#           'Start', 'True', 'MAP'], ['-b', '-r', '-g'], absolute=False)
+odlab.plot.residuals(post, [state0_named, true_state, post.results.MAP], [
+          'Start', 'True', 'MAP'], ['-b', '-r', '-g'], absolute=True)
 
 plt.show()
-
