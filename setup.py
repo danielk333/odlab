747961535dd0ca50a19333efce94b045683eba30
danielk <daniel.kastinen@irf.se>
2023-08-04 07:58:11 +0200

    moved to src config

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ import setuptools
 import pathlib
 import codecs
 
+# from distutils.core import Extension
+
 HERE = pathlib.Path(__file__).resolve().parents[0]
 
 
 def get_version(path):
-    with codecs.open(path, 'r') as fp:
+    with codecs.open(path, "r") as fp:
         for line in fp.read().splitlines():
-            if line.startswith('__version__'):
+            if line.startswith("__version__"):
                 delim = '"' if '"' in line else "'"
                 return line.split(delim)[1]
         else:
@@ -16,5 +18,8 @@ def get_version(path):
 
 
 setuptools.setup(
-    version=get_version(HERE / 'odlab' / 'version.py'),
+    version=get_version(HERE / "src" / "odlab" / "version.py"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    ext_modules=[],
 )
