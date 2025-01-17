fixed the autocovariance calculation and made example usage

src/odlab/statistics.py
@@ -1,112 +1,89 @@
+from typing import Optional
+import numpy as np
 
-def autocovariance(trace, max_k = None, min_k = None):
+
+def autocovariance(
+    chain: np.ndarray,
+    max_k: Optional[int] = None,
+    min_k: Optional[int] = None,
+) -> np.ndarray:
+    """Calculate the natural estimator of the autocovariance function of a Markov chain.
+
+    The autocovariance function is defined as
+    $$
+        \\gamma_k = cov(g(X_i), g(X_{i + k}))
+    $$
+    where $g$ is a real-valued function on the state space and $X_j$ is a sample of the
+    Markov chain in that state space.
+
+    The natural estimator of the autocovariance function and the mean is
+    $$
+        \\gamma_k = \\frac{1}{n} \\sum_{i=1}^{n - k} 
+            (g(X_i) - \\hat{\\mu}_n)(g(X_{i + k) - \\hat{\\mu}_n), \\\\
+        \\hat{\\mu}_n = \\frac{1}{n} \\sum_{i=1}^{n} g(X_i)
+    $$
+
+    Parameters
+    ----------
+    chain : np.ndarray
+        The Markov chain represented as a (N, M) matrix where N is the number of dimensions
+        and M is the number of steps
+    max_k : optional, int
+        The maximum autocorrelation length
+    min_k : optional, int
+        The minimum autocorrelation length
+
+    Returns
+    -------
+    float or numpy.ndarray
+        autocovariance function between $k_min$ and $k_max$ [1]
+
+    """
+    _n = chain.shape[1]
+    dims = chain.shape[0]
+
     if max_k is None:
-        max_k = len(self.trace)
+        max_k = _n
     else:
-        if max_k >= len(self.trace):
-            max_k = len(self.trace)
+        if max_k >= _n:
+            max_k = _n
 
     if min_k is None:
         min_k = 0
     else:
-        if min_k >= len(self.trace):
-            min_k = len(self.trace)-1
+        if min_k >= _n:
+            min_k = _n - 1
 
-    gamma = np.empty((max_k-min_k,), dtype=self.trace.dtype)
+    gamma = np.empty(
+        (
+            dims,
+            max_k - min_k,
+        ),
+        dtype=chain.dtype,
+    )
+    mu = np.mean(chain, axis=1)
 
-    _n = len(self.trace)
-
-    for var in self.variables:
+    for vari in range(dims):
         for k in range(min_k, max_k):
-            covi = self.trace[var][:(_n-k)] - self.MAP[0][var]
-            covik = self.trace[var][k:_n] - self.MAP[0][var]
-            gamma[var][k] = np.sum(covi*covik)/float(_n)
+            covi = chain[vari, :(_n - k)] - mu[vari]
+            covik = chain[vari, k:_n] - mu[vari]
+            gamma[vari, k] = np.sum(covi * covik) / float(_n)
 
     return gamma
 
-def batch_mean(self, batch_size):
-    if batch_size > len(self.trace):
-        raise Exception('Not enough samples to calculate batch statistics')
+
+def batch_mean(chain, batch_size):
+    _n = chain.shape[1]
+    dims = chain.shape[0]
+    if batch_size > _n:
+        raise Exception("Not enough samples to calculate batch statistics")
 
     _max = batch_size
-    batches = len(self.trace)//batch_size
-    batch_mean = np.empty((batches,), dtype=self.trace.dtype)
+    batches = _n // batch_size
+    batch_mean = np.empty((dims, batches,), dtype=chain.dtype)
     for ind in range(batches):
-        batch = self.trace[(_max - batch_size):_max]
+        batch = chain[:, (_max - batch_size):_max]
         _max += batch_size
-
-        for var in self.variables:
-            batch_mean[ind][var] = np.mean(batch[var])
+        batch_mean[:, ind] = np.mean(batch, axis=1)
 
     return batch_mean
-
-def batch_covariance(self, batch_size):
-    if batch_size > len(self.trace):
-        raise Exception('Not enough samples to calculate batch statistics')
-
-    batch_mean = self.batch_mean(batch_size)
-
-    _max_str = int(np.max([len(var) for var in self.variables]))
-
-    _dtype = self.trace.dtype.names
-    _dtype = [('variable', 'U{}'.format(_max_str))] + \
-        [(name, 'float64') for name in _dtype]
-    cov = np.empty((len(self.variables),), dtype=_dtype)
-    for ind, xvar in enumerate(self.variables):
-        for yvar in self.variables:
-            cov[ind]['variable'] = xvar
-            cov[ind][yvar] = np.mean((batch_mean[xvar] - self.MAP[xvar])*(
-                batch_mean[yvar] - self.MAP[yvar]))/float(len(batch_mean))
-
-    return cov
-
-def batch_variance(self, batch_size):
-    if batch_size > len(self.trace):
-        raise Exception('Not enough samples to calculate batch statistics')
-
-    batch_mean = self.batch_mean(batch_size)
-
-    variance = np.empty((1,), dtype=self.trace.dtype)
-    for var in self.variables:
-        variance[var] = np.mean((batch_mean[var] - self.MAP[var])**2)
-
-    return variance/float(len(batch_mean))
-
-def covariance_mat(self, variables=None):
-    if variables is None:
-        variables = self.variables
-
-    cov = np.empty((len(variables), len(variables)), dtype=np.float64)
-
-    mean = np.empty((1,), dtype=self.trace.dtype)
-    for ind, xvar in enumerate(variables):
-        mean[xvar] = np.mean(self.trace[xvar])
-
-    for xind, xvar in enumerate(variables):
-        for yind, yvar in enumerate(variables):
-            _var = (self.trace[xvar] - mean[xvar]) \
-                * (self.trace[yvar] - mean[yvar])
-            cov[xind, yind] = np.sum(_var)/float(len(self.trace)-1)
-    return cov
-
-def covariance(self):
-
-    _max_str = int(np.max([len(var) for var in self.variables]))
-
-    _dtype = self.trace.dtype.names
-    _dtype = [('variable', 'U{}'.format(_max_str))] + \
-        [(name, 'float64') for name in _dtype]
-    cov = np.empty((len(self.variables),), dtype=_dtype)
-
-    mean = np.empty((1,), dtype=self.trace.dtype)
-    for ind, xvar in enumerate(self.variables):
-        mean[xvar] = np.mean(self.trace[xvar])
-
-    for ind, xvar in enumerate(self.variables):
-        for yvar in self.variables:
-            cov[ind]['variable'] = xvar
-            _var = (self.trace[xvar] - mean[xvar]) \
-                * (self.trace[yvar] - mean[yvar])
-            cov[ind][yvar] = np.sum(_var)/float(len(self.trace)-1)
-    return cov
-